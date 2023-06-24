5ce7aae575c9029f0e8ac762161793ef99a77350
Vinicius Freire <113783249+Vinicius203@users.noreply.github.com>
2023-06-24 20:12:44 -0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Vinicius/Desktop/banco
-# Build directory: C:/Users/Vinicius/Desktop/banco/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
